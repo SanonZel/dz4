e8b36993ecea294c6383e01a69a91ed55c84b884
SanonZel <kanatovaha@gmail.com>
2022-11-17 22:58:17 +0600

    dz4

---
+++ app.js
@@ -0,0 +1,25 @@
+const som = document.querySelector("#som")
+const usd = document.querySelector("#dollar")
+
+const submit = (elem, target) => {
+    elem.addEventListener("input", () =>{
+        const request = new XMLHttpRequest()
+        request.open("GET", "data.json")
+        request.setRequestHeader('content-type', 'application/json')
+        request.send()
+        request.addEventListener('load', () => {
+        const respons = JSON.parse(request.response)
+            if (elem === som){
+                target.value = (elem.value / respons.som).toFixed(2)
+            }else if (elem === usd){
+                target.value = (elem.value * respons.som).toFixed(2)
+            }
+            elem.value === "" ? (target.value = '') : null
+        })
+    })    
+}
+ submit(som, usd)
+ submit(usd, som)
+
+    
+ 
